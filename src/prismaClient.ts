ba904836a1420b6d5cab2726160f59ebd3f522b0
Ahmed Hussein <ahmedgg12347800@gmail.com>
2026-05-31 20:48:33 +0300

    fix prisma mongodb

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,16 +1,11 @@
-// import { PrismaClient } from '@prisma/client';
-// import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-
-// const adapter = new PrismaBetterSqlite3({
-//   url: process.env.DATABASE_URL || 'file:./dev.db'
-// });
-
-// const prisma = new PrismaClient({ adapter } as any);
-
-// export default prisma;
-
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
