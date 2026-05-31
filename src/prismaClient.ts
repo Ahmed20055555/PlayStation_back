0549e8a8c5f197846fb02c5107d7aca07b41fb80
Ahmed Hussein <ahmedgg12347800@gmail.com>
2026-05-31 20:30:40 +0300

    add file

--- src/prismaClient.ts
+++ src/prismaClient.ts
@@ -1,10 +1,16 @@
+// import { PrismaClient } from '@prisma/client';
+// import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+
+// const adapter = new PrismaBetterSqlite3({
+//   url: process.env.DATABASE_URL || 'file:./dev.db'
+// });
+
+// const prisma = new PrismaClient({ adapter } as any);
+
+// export default prisma;
+
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL || 'file:./dev.db'
-});
-
-const prisma = new PrismaClient({ adapter } as any);
+const prisma = new PrismaClient();
 
 export default prisma;
